4628b78c887a67939fe57c9fca8cdb7cbe548ea9
Jableed43 <javiernehuenlopez@gmail.com>
2023-01-04 18:14:27 -0300

    cambio en build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 #Los requirements son creados pip freeze > requeriments.txt
 #Instala las dependencias necesarias que se encuentran en build.sh
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
